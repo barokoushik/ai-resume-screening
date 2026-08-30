21afca11c930ee9628e867e4f293233a7b203635
barokoushik <barokoushik4@gmail.com>
2026-08-30 05:33:37 +0000

    feat: add how it works and about pages

--- app.py
+++ app.py
@@ -342,6 +342,11 @@ def home():
         candidate_results=candidate_results
     )
 
-
+@app.route("/how-it-works")
+def how_it_works():
+    return render_template("how_it_works.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
 if __name__ == "__main__":
     app.run(debug=True)
